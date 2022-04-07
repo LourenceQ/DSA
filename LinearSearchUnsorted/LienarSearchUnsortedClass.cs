d3926b6ba60f2c06fcc4d59b6382db05aa37f39e
LourenceQ <lqfmecatronica@gmail.com>
2022-04-07 16:20:38 -0300

    Initial commit

---
+++ LinearSearchUnsorted/LienarSearchUnsortedClass.cs
@@ -0,0 +1,32 @@
+﻿namespace LienarSearchUnsorted
+{
+
+    /// <summary>
+    /// this is a class library with an unordered linear array search method
+    /// </summary>
+    public class LienarSearchUnsortedClass
+    {
+        public LienarSearchUnsortedClass(int[] sortedArr, int value)
+        {
+            SortedArr = sortedArr;
+            Value = value;
+        }
+
+        public int[] SortedArr { get; private set; }
+        
+        public int Value { get; private set; }        
+
+        public bool LinearSearchMethod()
+        {
+            for (int i = 0; i < this.SortedArr.Length; i++)
+            {
+                if (this.Value == this.SortedArr[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }        
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LienarSearchUnsorted;
+namespace SearchAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 45, 3, 78, 9, 3, 4 };
+            Array.Sort(arr);
+            int value = 4;
+
+            var linearSearch = new LienarSearchUnsortedClass(arr, value);
+            linearSearch.LinearSearchMethod().ToString();
+
+        }
+    }
+}
