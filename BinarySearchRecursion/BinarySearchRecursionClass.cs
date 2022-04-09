e8dcf8fac3b2612ab1c4878994372fc3e94bb20c
LourenceQ <lqfmecatronica@gmail.com>
2022-04-09 12:19:28 -0300

    binary Search with recursion

---
+++ BinarySearchRecursion/BinarySearchRecursionClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BinarySearchRecursion
+{
+    public class BinarySearchRecursionClass
+    {
+        public bool BinarySearchRecursionMethod(int[] Arr, int size, int Value)
+        {
+            int left = 0;
+            int right = size - 1;
+            return BinarySearchRecursionMethodUtil(Arr, left, right, Value);
+        }
+
+        public static bool BinarySearchRecursionMethodUtil(int[] Arr, int left, int right, int Value)
+        {
+            if (left > right)
+                return false;
+
+            int mid = (left + right) / 2;
+
+            if (Arr[mid] == Value)
+                return true;
+
+            else if (Arr[mid] < Value)
+                return BinarySearchRecursionMethodUtil(Arr, mid + 1, right, Value);
+
+            else
+            {
+                return BinarySearchRecursionMethodUtil(Arr, left, mid - 1, Value);
+            }
+        }
+
+
+    }
+}
