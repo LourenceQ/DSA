a087d51e7f16e9620830d5149ba7426d008df67b
LourenceQ <lqfmecatronica@gmail.com>
2022-06-22 12:59:43 -0300

    implementando método de contagem de duraçao de tempo de execução de algorítmo

--- Main/Program.cs
+++ Main/Program.cs
@@ -6,17 +6,35 @@ using FirstRepeatedElementInArray;
 using static System.Console;
 using RemoveDuplicatesInIntegerList;
 using FindMissingNumberInIntegerList;
+using System;
+using System.Diagnostics;
 
 namespace SearchAlgorithms
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
             int[] arr = { 1, 45, 3, 78, 9, 3, 4 };
             int[] arr2 = { 1, 3, 3, 4, 9, 43, 78 };
             int[] arr3 = { 1, 45, 3, 78, 9, 3, 4, 1, 45 };
             int[] arr4 = new int[] { 34, 56, 77, 1, 5, 6, 6, 6, 6, 6, 6, 7, 8, 10, 34, 20 };
+            int[] nums = new int[100000];
+
+            BuildArray(nums);
+
+            TimeSpan startTime;
+            TimeSpan duration;
+            
+            startTime = Process.GetCurrentProcess()
+                .Threads[0].UserProcessorTime;
+
+            DisplayNums(nums);
+
+            duration = Process.GetCurrentProcess()
+                .Threads[0].UserProcessorTime.Subtract(startTime);
+
+            Console.WriteLine("Time: " + duration.TotalSeconds);
 
             int value = 4;
             int value2 = 78;
@@ -54,10 +72,19 @@ namespace SearchAlgorithms
                 $"BinarySearchRecursion: {res4}\n" +
                 $"FirstRepeatedElementBruteForce: {res5}\n" +
                 $"Find Missing Number Solution1: {res7}");
-                
+
             FirstRepeatedHashTable.FirstRepeatedHash(arr3);
 
-
+        }
+        static void BuildArray(int[] arr)
+        {
+            for (int i = 0; i <= 99999; i++)
+                arr[i] = i;
+        }
+        static void DisplayNums(int[] arr)
+        {
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+                Console.Write(arr[i] + " ");
         }
     }
 }
